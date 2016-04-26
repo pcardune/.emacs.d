de4726459c9036cfc6462b00c98988bc780f2c67
Paul Carduner <paul@carduner.net>
2016-04-26 11:58:38 -0700

    Use existing buffer prefix as prompt default

--- settings/custom-functions.el
+++ settings/custom-functions.el
@@ -1,6 +1,16 @@
-; "Open a multi term and prompt for buffer name right away"
 (defun paul-term (buffer-name)
-  (interactive "sterminal name: ")
+  "Open a multi term and prompt for buffer name right away"
+  (interactive
+   (list
+    (read-string "Open new terminal (with buffer new name): "
+                 (if (string-match-p ":" (buffer-name))
+                     (format "%s:" (first (split-string (buffer-name) ":")))
+                   ""
+                   )
+                 nil
+                 "foo")
+    )
+   )
   (multi-term)
   (rename-buffer buffer-name)
   )
